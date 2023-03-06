44dc41848921e6dc4017af7c9cd76fec61e8fabb
tal-diamant <erkeliwood@gmail.com>
2023-03-06 19:02:47 +0700

    feat: put better placeholders in benefits section

--- src/sections/benefits/Benefits.tsx
+++ src/sections/benefits/Benefits.tsx
@@ -31,33 +31,33 @@ export default function Benefits() {
           </div>
 
           <div className={styles.benefit}>
-            <Image src="/images/infinity.svg" width={100} height={100} alt="" />
-            <h3>Design board</h3>
-            <p>Add as many design requests to your board as you'd like.</p>
+            <Image src="/images/benefits-lightning.svg" width={100} height={100} alt="" />
+            <h3>Lightning fast delivery</h3>
+            <p>Get your design one at a time in just a few days on average.</p>
           </div>
 
           <div className={styles.benefit}>
-            <Image src="/images/infinity.svg" width={100} height={100} alt="" />
-            <h3>Design board</h3>
-            <p>Add as many design requests to your board as you'd like.</p>
+            <Image src="/images/benefits-money.svg" width={100} height={100} alt="" />
+            <h3>Fixed monthly rate</h3>
+            <p>No surprises here! Pay the same fixed price each month.</p>
           </div>
 
           <div className={styles.benefit}>
-            <Image src="/images/infinity.svg" width={100} height={100} alt="" />
-            <h3>Design board</h3>
-            <p>Add as many design requests to your board as you'd like.</p>
+            <Image src="/images/benefits-painter.svg" width={100} height={100} alt="" />
+            <h3>Top-notch quality</h3>
+            <p>Insane design quality at your fingertips whenever you need it.</p>
           </div>
 
           <div className={styles.benefit}>
-            <Image src="/images/infinity.svg" width={100} height={100} alt="" />
-            <h3>Design board</h3>
-            <p>Add as many design requests to your board as you'd like.</p>
+            <Image src="/images/benefits-scale.svg" width={100} height={100} alt="" />
+            <h3>Flexible and scalable</h3>
+            <p>Scale up or down as needed, and pause or cancel at anytime.</p>
           </div>
 
           <div className={styles.benefit}>
-            <Image src="/images/infinity.svg" width={100} height={100} alt="" />
-            <h3>Design board</h3>
-            <p>Add as many design requests to your board as you'd like.</p>
+            <Image src="/images/benefits-fingerprint.svg" width={100} height={100} alt="" />
+            <h3>Unique and all yours</h3>
+            <p>Each of your designs is made especially for you and is 100% yours.</p>
           </div>
         </div>
         </AnimateIn>
